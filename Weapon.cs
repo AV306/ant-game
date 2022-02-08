8dc00ac5be093ad1b107e5381d968b4bb4e1c73a
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-02-08 10:20:30 +0800

    Intermediate

---
+++ Weapon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+	public int type = 0;
+
+	public const int HAND = 0;
+	public const int NEWSPAPER = 1;
+	public const int BAGETT = 2;
+
+	public void Use( Vector2 target )
+	{
+		
+	}
+}
